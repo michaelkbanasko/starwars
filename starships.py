12c770ab32b990b14043b507fa3e4ae6789ba341
Michael Kola Banasko <kolapobanasko@gmail.com>
2022-04-13 08:27:11 +0100

    second commit

--- starships.py
+++ starships.py
@@ -3,16 +3,19 @@ from pprint import pprint
 import requests
 import json
 
+#connected to the database
 client = pymongo.MongoClient()
 db = client["starwars"]
 
+#emptied the previous data in the database
 db.starships.drop()
 
+#created a function, method to get updated id
 def get_id(st):
     lurls = st['pilots']
     pilots_name = []
     object_ids = []
-    for url in lurls:
+    for url in lurls: #ging through each url in pilot and get the objectid
         pilot = requests.get(url).json()['name']
         pilots_name.append(pilot)
         obj = db.characters.find_one({'name': pilot})['_id']
@@ -22,26 +25,28 @@ def get_id(st):
     # print( pilots_name)
     return object_ids
 
-
+# function to loop through all the pages and get the data
 def get_all_data(url):
     result = []
     json_return = requests.get(url).json()
     result.extend(json_return['results'])
     next_url = json_return['next']
-    while next_url:
+    while next_url: #keeps looping through the different pages
         json_return = requests.get(next_url).json()
         result.extend(json_return['results'])
         next_url = json_return['next']
 
     return result
 
-
-data = get_all_data("https://swapi.dev/api/starships/")
+# get the data from api
+api = "https://swapi.dev/api/starships/"
+data = get_all_data(api)
 # results["pilots"]
 
+# loop through each starship and update with objectId
 for st in data:
     ids = get_id(st)
     st['pilots'] = ids
 
-
+# Add this to the starships collection
 db.starships.insert_many(data)
